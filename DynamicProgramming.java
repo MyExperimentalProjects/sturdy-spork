e1b6c2d775236ed07c91c5a6817bee914e3b5a8d
Harshita Kasera <hkasera5@gmail.com>
2016-10-08 20:26:41 -0700

    Practice DP

---
+++ DynamicProgramming.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class DynamicProgramming{
+
+	public static int fib(int n){
+		if( n == 0 || n == 1){
+			return n;
+		}
+		int[] memo = new int[n+1];
+		memo[0] = 0;
+		memo[1] = 1;
+		for(int i = 2; i <= n ; ++i){
+			memo[i] = memo[i-1] + memo[i-2];
+		}
+		return memo[n];
+	}
+
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		System.out.println(fib(n));
+	}
+}
